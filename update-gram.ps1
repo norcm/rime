e10ae54ae95cbc65059f46883cd5720d55f7ab4f
gaia <gaia@norcm>
2026-04-27 22:13:10 +0800

    update

--- update-gram.ps1
+++ update-gram.ps1
@@ -3,7 +3,7 @@ Set-StrictMode -Version Latest
 
 $RepoDir = $PSScriptRoot
 $ConfigurationDir = Join-Path $RepoDir 'configuration'
-$GramFile = Join-Path $ConfigurationDir 'wanxiang-lts-zh-hans.gram'
+$GramFile = Join-Path $ConfigurationDir 'wanxiang-lts-zh-hans.gram-new'
 $GramUrl = 'https://github.com/amzxyz/RIME-LMDG/releases/download/LTS/wanxiang-lts-zh-hans.gram'
 
 Invoke-WebRequest -Uri $GramUrl -OutFile $GramFile
